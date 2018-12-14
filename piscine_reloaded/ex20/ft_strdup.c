685298dbc7632772c448c06cc3c54038943caf29
N0panda <yohan.thomas27@hotmail.com>
2018-12-14 17:21:46 +0100

    push des projets

---
+++ piscine_reloaded/ex20/ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ythomas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 15:33:38 by ythomas           #+#    #+#             */
+/*   Updated: 2018/11/07 15:55:09 by ythomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		i;
+
+	if (!(str = (char*)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
